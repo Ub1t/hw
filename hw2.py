d280c1601e1af5373cca099fb34b2b260882d829
Ub1t <44301460+Ub1t@users.noreply.github.com>
2018-10-20 13:48:53 +0300

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -2,7 +2,7 @@ import math
 x = float(input("Введите x: "))
 PI = math.pi
 if -PI <= x <= PI:
-    print("y =", math.sin(3 * x))
+    print("y =", math.cos(3 * x))
 elif -PI > x > PI:
     print("y =", x)
    
