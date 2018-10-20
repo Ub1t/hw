337b9888b91f618a0924681d727be986cdec28cc
Ub1t <44301460+Ub1t@users.noreply.github.com>
2018-10-20 13:40:33 +0300

    Create h6.py

---
+++ h6.py
@@ -0,0 +1,16 @@
+number = 6
+text = "нет результата"
+if number == 0:
+    print(number)
+elif number == 1:
+    print(number)
+elif number == 2:
+    print(number)
+elif number == 3:
+    print(number)
+elif number == 4:
+    print(number)
+elif number == 5:
+    print(number)
+else:
+    print("Ошибка!", text + "!")
